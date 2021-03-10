7c1d4875fda53bb72e92ab5fece7fe1c0b11a7cd
Marvin <aguirreestuardo758@gmail.com>
2021-03-09 19:52:48 -0600

    Se realizaron distintos cambios

--- Rueda_De_laFortuna/src/rueda_de_lafortuna/Rueda_De_laFortuna.java
+++ Rueda_De_laFortuna/src/rueda_de_lafortuna/Rueda_De_laFortuna.java
@@ -16,23 +16,24 @@ public class Rueda_De_laFortuna {
         int a;
         
         String[] palabras={"TELEFONO","MOTOCICLETA","AUTOMOVIL DEL AÑO","COMPUTADORA","Q. 10,000.00"};
-        do{
-            System.out.println("\nBIENVENIDO A LA RULETA DE LA FORTUNA!!! \n1-Dar vuelta a la ruleta  \n2. Retirarme del juego"
-                    + "\nSelecciona tu opcion:");
-            a=sc.nextInt();
-            switch(a){
-                case 1:
-                    System.out.println("TU PREMIO ES:  "+palabras[date]+" FELICIDADES!!!");
-                    break;
-                case 2:
-                    System.out.println("Hasta pronto");
-                    break;
-                default:
-                    System.out.println("ERROR 404 xD");
-                    break;
+        
+        System.out.println("\nBIENVENIDO A LA RULETA DE LA FORTUNA!!!");
+        System.out.println("LOS PREMIOS SON LOS SIGUIENTES: ");
+        System.out.println("TELEFONO, MOTOCICLETA, AUTOMOVIL DEL AÑO, COMPUTADORA, Q. 10,000.00\n");
+        System.out.println("Deseas jugar?\n1. Si   2. No");
+        a=sc.nextInt();
+        switch(a){
+            case 1:
+                System.out.println("TU PREMIO ES:  "+palabras[date]+" FELICIDADES!!!");
+                break;
+            case 2:
+                System.out.println("Hasta pronto");
+                break;
+            default:
+                System.out.println("ERROR 404 xD");
+                break;
             }
             
-        }while(a!=2);     
     }
     
 }
