0ecd169c1897b6bf70461653d1b1d87659dfe5ff
Marvin <aguirreestuardo758@gmail.com>
2021-04-23 18:33:57 -0600

    Se agregaron nuevos archivos

---
+++ Swing_1/src/swing_1/Swing_1.java
@@ -0,0 +1,13 @@
+
+package swing_1;
+
+
+public class Swing_1 {
+
+    public static void main(String[] args) {
+       Estudiante x =new Estudiante();
+       
+       x.setVisible(true);
+    }
+    
+}
