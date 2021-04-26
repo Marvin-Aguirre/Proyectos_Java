b7173e788e68052772a09e5d421a40a9b0025fee
Marvin <aguirreestuardo758@gmail.com>
2021-04-25 22:04:36 -0600

    Se realizaron cambios

---
+++ NecFlis_Progra1/src/necflis_progra1/Date.java
@@ -0,0 +1,33 @@
+
+package necflis_progra1;
+
+
+public class Date {
+    //CLIENTES
+    public String Nombre_Cliente;
+    public String Direccion_Cliente;
+    public int Edad_Cliente;
+    
+
+    public Date (String Nombre_Cliente,String Direccion_Cliente, int Edad_Cliente) {
+        this.Nombre_Cliente=Nombre_Cliente;
+        this.Direccion_Cliente=Direccion_Cliente; 
+        this.Edad_Cliente=Edad_Cliente;
+    }
+}
+class pelicula{
+    public String Nombre_Pelicula;
+    public String Tipo_Pelicula;
+    public String Genero_Pelicula;
+    public String Sinopsis_Pelicula;
+    
+    public pelicula(String Nombre_Pelicula,String Tipo_Pelicula,String Genero_Pelicula, String Sinopsis_Pelicula){
+        this.Nombre_Pelicula=Nombre_Pelicula;
+        this.Tipo_Pelicula=Tipo_Pelicula;
+        this.Genero_Pelicula=Genero_Pelicula;
+        this.Sinopsis_Pelicula=Sinopsis_Pelicula;
+    }
+}
+class playList{
+
+}
--- NecFlis_Progra1/src/necflis_progra1/NecFlis_Progra1.java
+++ NecFlis_Progra1/src/necflis_progra1/NecFlis_Progra1.java
@@ -1,21 +1,309 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package necflis_progra1;
 
-/**
- *
- * @author USUARIO
- */
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class NecFlis_Progra1 {
 
-    /**
-     * @param args the command line arguments
-     */
+    public static int selectToPeli;
+    public static int dato;
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        ArrayList nombreArray = new ArrayList();
+        ArrayList direccionArray = new ArrayList();
+        ArrayList edadArray = new ArrayList();
+        ArrayList nombrePeliculaArray = new ArrayList();
+        ArrayList tipoPeliculaArray = new ArrayList();
+        ArrayList generoPeliculaArray = new ArrayList();
+        ArrayList sinopsisPeliculaArray = new ArrayList();
+        int Opcion_Usuario, Opcion_Cliente, Opcion_SeleccionCliente, Opcion_Peliculas;
+        int selectToClient = 0, selectToPeli, cli = 0, contadorPeli = 0;
+        String cancion1 = "";
+
+        try {
+            do {
+
+                System.out.print("    |    Bienvenidos A NecFlis   | \n\n"
+                        + "1. Clientes \n2. Peliculas  \n3. Salir \n"
+                        + " Selecciona una de las opciones: ");
+                Opcion_Usuario = Integer.parseInt(sc.nextLine());
+
+                switch (Opcion_Usuario) {
+                    case 1:
+
+                        System.out.println("\n1. Ver listado de clientes\n2. Crear nuevo cliente\n"
+                                + "3. Seleccionar cliente\n4. Atras");
+                        Opcion_Cliente = Integer.parseInt(sc.nextLine());
+
+                        switch (Opcion_Cliente) {
+                            case 1:
+
+                                if (cli == 0) {
+                                    System.out.println("");
+                                    System.out.println("---------------------------");
+                                    System.out.println("No hay clientes en la lista");
+                                    System.out.println("---------------------------");
+                                    break;
+                                } else {
+                                    System.out.println("Listado de clientes:");
+                                    System.out.println("------------------------------------------------------------");
+                                    for (int j = 0; j < nombreArray.size(); j++) {
+                                        String cancion = (String) nombreArray.get(j);
+                                        String dir = (String) direccionArray.get(j);
+                                        int l = (int) edadArray.get(j);
+                                        System.out.println("NOMBRE: " + cancion + " DIRECCION: " + dir + " EDAD: " + l);
+                                        System.out.println("-----------------------------------------------------------");
+                                    }
+                                }
+
+                                break;
+                            case 2:
+
+                                cli++;
+                                System.out.println("CREAR NUEVO CLIENTE");
+                                int op;
+                                do {
+                                    System.out.println("Desea agregar cliente");
+                                    System.out.println("1. SI   2. NO");
+                                    op = Integer.parseInt(sc.nextLine());
+                                    int a = 0;
+                                    if (op == 1) {
+                                        a++;
+                                        Date[] listaEstudiantes = new Date[a];
+                                        int cont = 0;
+                                        while (cont < a) {
+                                            System.out.println("");
+                                            System.out.print("Ingresar el nombre: ");
+                                            String Nombre_Cliente = sc.nextLine();
+                                            System.out.print("Ingresar direccion: ");
+                                            String Direccion_Cliente = sc.nextLine();
+                                            System.out.print("Ingresar edad: ");
+                                            int Edad_Cliente = Integer.parseInt(sc.nextLine());
+                                            Date estudiante = new Date(Nombre_Cliente, Direccion_Cliente, Edad_Cliente);
+                                            listaEstudiantes[cont] = estudiante;
+                                            cont++;
+                                            System.out.println("Cliente almacenado exitosamente\n");
+                                        }
+                                        System.out.println("Listado de clientes:");
+                                        System.out.println("------------------------------------------------------------");
+                                        for (int i = 0; i < a; i++) {
+                                            Date estudiante = listaEstudiantes[i];
+                                            //System.out.println("NOMBRE CLIENTE: "+estudiante.Nombre_Cliente+" DIRECCION CLIENTE: "+estudiante.Direccion_Cliente+"EDAD:"+estudiante.Edad_Cliente);
+                                            nombreArray.add(estudiante.Nombre_Cliente);
+                                            direccionArray.add(estudiante.Direccion_Cliente);
+                                            edadArray.add(estudiante.Edad_Cliente);
+                                            for (int j = 0; j < nombreArray.size(); j++) {
+                                                String cancion = (String) nombreArray.get(j);
+                                                String dir = (String) direccionArray.get(j);
+                                                int l = (int) edadArray.get(j);
+                                                System.out.println("Nombre: " + cancion + " Direccion: " + dir + " Edad: " + l);
+                                                System.out.println("--------------------------------------------------------");
+                                            }
+                                        }
+
+                                    }
+
+                                } while (op != 2);
+
+                                break;
+                            case 3:
+
+                                System.out.println("SELECCIONAR UN CLIENTE:");
+
+                                //do {
+                                System.out.println("Listado de clientes:");
+                                System.out.println("------------------------------------------------------------");
+
+                                for (int j = 0; j < nombreArray.size(); j++) {
+                                    String cancion = (String) nombreArray.get(j);
+                                    System.out.println((j + 1) + "." + "  NOMBRE: " + cancion);
+                                }
+
+                                if (cli == 0) {
+                                    System.out.println("");
+                                    System.out.println("-------------------------------------------");
+                                    System.out.println("La lista de los clientes se encuentra vacia");
+                                    System.out.println("-------------------------------------------");
+                                    System.out.println("");
+                                } else {
+                                    System.out.println("Elegir el cliente deseado");
+                                    selectToClient = Integer.parseInt(sc.nextLine());
+
+                                    for (int j = 0; j < nombreArray.size(); j++) {
+                                        cancion1 = (String) nombreArray.get(selectToClient - 1);
+
+                                    }
+                                    System.out.println("Nombre: " + cancion1);
+                                }
+
+                                do {
+                                    System.out.print("1. PlayList\n2. Agregar pelicula\n3. Atras\n"
+                                            + "Selecciona una de las opciones: ");
+                                    Opcion_SeleccionCliente = Integer.parseInt(sc.nextLine());
+
+                                    switch (Opcion_SeleccionCliente) {
+                                        case 1:
+                                            System.out.println("PlayList de: " + cancion1);
+                                            System.out.println("No. " + (dato - 1) + " Nombre de la pelicula " + nombrePeliculaArray.get(dato - 1) + " genero " + generoPeliculaArray.get(dato - 1) + " tipo " + tipoPeliculaArray.get(dato - 1) + " sinopsis " + sinopsisPeliculaArray.get(dato - 1));
+                                            System.out.println("");
+
+                                            break;
+                                        case 2:
+                                            System.out.println("LISTADO DE PELICULAS");
+                                            for (int j = 0; j < nombrePeliculaArray.size(); j++) {
+                                                String nomPel = (String) nombrePeliculaArray.get(j);
+                                                String tipoPel = (String) tipoPeliculaArray.get(j);
+                                                String generoPel = (String) generoPeliculaArray.get(j);
+                                                String sinopsisPel = (String) sinopsisPeliculaArray.get(j);
+
+                                                System.out.println("-----------------------------------------------");
+                                                System.out.println((j + 1) + ".");
+                                                System.out.println("Nombre: " + nomPel);
+                                                System.out.println("Tipo: " + tipoPel);
+                                                System.out.println("Genero: " + generoPel);
+                                                System.out.println("Sinopsis: " + sinopsisPel);
+                                                System.out.println("------------------------------------------------");
+                                            }
+
+                                            System.out.println("");
+                                            System.out.println("Elija una pelicula: ");
+                                            int dap = Integer.parseInt(sc.nextLine());
+                                            if (dap == 0) {
+                                                break;
+                                            } else {
+                                                System.out.println("Pos. " + (dap - 1) + " Nombre de la pelicula  " + nombrePeliculaArray.get(dap - 1) + " genero " + generoPeliculaArray.get(dap - 1) + " tipo " + tipoPeliculaArray.get(dap - 1) + " sinopsis " + sinopsisPeliculaArray.get(dap - 1));
+                                                System.out.println("");
+                                                dato = dap;
+                                            }
+                                            break;
+                                        case 3:
+                                            break;
+                                        default:
+                                            System.out.println("Opcion incorrecta");
+                                            break;
+
+                                    }
+
+                                } while (Opcion_SeleccionCliente != 3);
+
+                                break;
+                            case 4:
+                                break;
+                        }
+
+                        break;
+                    case 2:
+
+                        do {
+
+                            System.out.print("\n1. Ver listado de peliculas\n2. Crear pelicula\n3. Atras\n"
+                                    + "Selecciona una de las opciones: ");
+                            Opcion_Peliculas = Integer.parseInt(sc.nextLine());
+                            switch (Opcion_Peliculas) {
+                                //LISTADO DE PELICULAS
+                                case 1:
+                                    if (contadorPeli == 0) {
+                                        System.out.println("");
+                                        System.out.println("----------------------------------------");
+                                        System.out.println("La lista de peliculas se encuentra vacia");
+                                        System.out.println("-----------------------------------------");
+                                    } else {
+
+                                        System.out.println("Listado de peliculas");
+                                        for (int j = 0; j < nombrePeliculaArray.size(); j++) {
+                                            String nomPel = (String) nombrePeliculaArray.get(j);
+                                            String tipoPel = (String) tipoPeliculaArray.get(j);
+                                            String generoPel = (String) generoPeliculaArray.get(j);
+                                            String sinopsisPel = (String) sinopsisPeliculaArray.get(j);
+                                            System.out.println("-----------------------------------------------");
+                                            System.out.println((j + 1) + "");
+                                            System.out.println("Nombre: " + nomPel);
+                                            System.out.println("Tipo: " + tipoPel);
+                                            System.out.println("Genero: " + generoPel);
+                                            System.out.println("Sinopsis: " + sinopsisPel);
+                                            System.out.println("------------------------------------------------");
+                                        }
+                                    }
+                                    break;
+                                case 2:
+                                    System.out.println("Crear Pelicula");
+                                    int op;
+                                    do {
+                                        System.out.println("Desea agregar una pelicula");
+                                        System.out.println("1. SI   2. NO");
+                                        op = Integer.parseInt(sc.nextLine());
+                                        int a = 0;
+                                        if (op == 1) {
+                                            a++;
+                                            pelicula[] listaPeliculas = new pelicula[a];
+                                            int cont = 0;
+                                            while (cont < a) {
+                                                System.out.println("");
+                                                System.out.print("Ingresar el nombre: ");
+                                                String Nombre_Pelicula = sc.nextLine();
+                                                System.out.print("Ingresar tipo (SERIE/PELICULA): ");
+                                                String Tipo_Pelicula = sc.nextLine();
+                                                System.out.print("Ingresar genero  (drama, Comedia, terror, suspenso): ");
+                                                String Genero_Pelicula = sc.nextLine();
+                                                System.out.print("Ingresar sinopsis: ");
+                                                String Sinopsis_Pelicula = sc.nextLine();
+
+                                                pelicula cliente = new pelicula(Nombre_Pelicula, Tipo_Pelicula, Genero_Pelicula, Sinopsis_Pelicula);
+                                                listaPeliculas[cont] = cliente;
+                                                cont++;
+                                                contadorPeli++;
+                                            }
+                                            //System.out.println("LISTAS********");
+                                            for (int i = 0; i < a; i++) {
+                                                pelicula cliente = listaPeliculas[i];
+                                                //System.out.println("NOMBRE CLIENTE: "+estudiante.Nombre_Cliente+" DIRECCION CLIENTE: "+estudiante.Direccion_Cliente+"EDAD:"+estudiante.Edad_Cliente);
+                                                nombrePeliculaArray.add(cliente.Nombre_Pelicula);
+                                                tipoPeliculaArray.add(cliente.Tipo_Pelicula);
+                                                generoPeliculaArray.add(cliente.Genero_Pelicula);
+                                                sinopsisPeliculaArray.add(cliente.Sinopsis_Pelicula);
+                                            }
+                                            System.out.println("Listado de peliculas");
+                                            for (int j = 0; j < nombrePeliculaArray.size(); j++) {
+                                                String nomPel = (String) nombrePeliculaArray.get(j);
+                                                String tipoPel = (String) tipoPeliculaArray.get(j);
+                                                String generoPel = (String) generoPeliculaArray.get(j);
+                                                String sinopsisPel = (String) sinopsisPeliculaArray.get(j);
+                                                System.out.println("-----------------------------------------------");
+                                                System.out.println((j + 1) + "");
+                                                System.out.println("Nombre: " + nomPel);
+                                                System.out.println("Tipo: " + tipoPel);
+                                                System.out.println("Genero: " + generoPel);
+                                                System.out.println("Sinopsis: " + sinopsisPel);
+                                                System.out.println("------------------------------------------------");
+                                            }
+
+                                        }
+
+                                    } while (op != 2);
+                                    break;
+                                case 3:
+
+                                    break;
+                                default:
+                                    System.out.println("OPCION NO DECLARADA EN EL MENU");
+                                    break;
+                            }
+                        } while (Opcion_Peliculas != 3);
+
+                        break;
+                    case 3:
+                        System.out.println("Programa finalizado");
+                        break;
+                    default:
+                        System.out.println("Opcion Invalida");
+                        break;
+                }
+
+            } while (Opcion_Usuario != 3);
+
+        } catch (Exception excepcion) {
+            System.out.println("Error");
+        }
     }
-    
 }
